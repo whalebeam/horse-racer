09fe49fda34d90c62a27966ebfe06fb752545c7b
Dave <buzzpunch@outlook.com>
2020-09-05 14:40:35 +0100

    race-list: Add Race types

--- horse-racer/Races/Race.swift
+++ horse-racer/Races/Race.swift
@@ -5,22 +5,30 @@
 
 import Foundation
 
-//struct Race: Decodable {
-//    let name: String
-//    let course_name: String
-//    let age: String
-//    let distance: String
-//    let time: String // TODO: Change this
-//    let ride_count: Int
-//    let race_stage: String
-//    let going: String
-//    let has_handicap: Bool
-//    let hidden: Bool
-//}
-
-
-// TODO: undo temp structure
 struct Race: Decodable {
-    let name: String?
-    let page: Int
+    let name: String
+    let course_name: String
+    let age: String
+    let distance: String = ""
+    let time: String // TODO: Change this
+    let ride_count: Int = 0
+    let race_stage: String = ""
+    let going: String = ""
+    let has_handicap: Bool = false
+    let hidden: Bool = false
+    let rides: [Ride] = []
+}
+
+
+struct Ride: Decodable {
+    let cloth_number: Int
+    let horse: Horse
+}
+
+
+struct Horse: Decodable {
+    let name: String
+    let days_since_last_run: Int
+    let foaled: String // could be date type
+    let sex: String
 }
