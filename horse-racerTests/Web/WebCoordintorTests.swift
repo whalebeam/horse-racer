4ecc7df96ef70c9e3397b9881946239bca95fd31
Dave <buzzpunch@outlook.com>
2020-09-07 22:35:43 +0100

    Add web coordinator tests, remove general networkservice as redundant

--- horse-racerTests/Web/WebCoordintorTests.swift
+++ horse-racerTests/Web/WebCoordintorTests.swift
@@ -11,8 +11,21 @@ final class WebCoordinatorTests: XCTestCase {
     
     func test_start() {
         
-//        let sut = WebCoordinator(navigationController: <#T##UINavigationController#>, url: <#T##URL#>)
         
+        let nav = UINavigationController()
+        let sut = WebCoordinator(navigationController: nav, url: URL(string: "https://duckduckgo.com")!)
+        let promise = expectation(description: "I expect not to be called because viewDidLoad has not been called since the navigation controller isn't on screen")
+        promise.isInverted = true
+        
+        sut.viewController.load = { _ in
+            promise.fulfill()
+            return .none
+        }
+        
+        sut.start()
+        
+        XCTAssertEqual(nav.viewControllers.count, 1)
+        waitForExpectations(timeout: 1.0, handler: nil)
     }
     
 }
